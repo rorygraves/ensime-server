aemoncannon/ensime

Further work on RPC, update workflow tests

build.sbt
@@ -46,8 +46,7 @@ libraryDependencies ++= Seq(
   "ch.qos.logback"             %  "logback-classic"      % "1.1.2",
   "org.slf4j"                  %  "jul-to-slf4j"         % "1.7.7",
   "org.slf4j"                  %  "jcl-over-slf4j"       % "1.7.7",
-  "org.scala-refactoring"      %% "org.scala-refactoring.library" % "0.6.2",
-  "com.chuusai"                %% "shapeless"            % "2.0.0"
+  "org.scala-refactoring"      %% "org.scala-refactoring.library" % "0.6.2"
 )
 
 // WORKAROUND: https://github.com/typelevel/scala/issues/75
@@ -147,7 +146,7 @@ scalariformSettings
 instrumentSettings
 
 // let's bump this every time we get more tests
-ScoverageKeys.minimumCoverage := 64
+ScoverageKeys.minimumCoverage := 73
 
 // might be buggy
 ScoverageKeys.highlighting := true

src/main/scala/org/ensime/protocol/swank/SwankWireFormatCodec.scala
@@ -1,0 +1,54 @@
+package org.ensime.protocol.swank
+
+import java.io.{ EOFException, OutputStream }
+
+import org.ensime.protocol.ProtocolWireFormatCodec
+import org.ensime.util.{ SExpParser, WireFormat }
+import org.slf4j.Logger
+
+import scala.util.parsing.input
+
+trait SwankWireFormatCodec extends ProtocolWireFormatCodec {
+
+  val log: Logger
+
+  // Handle reading / writing of messages
+  override def writeMessage(value: WireFormat, out: OutputStream): Unit = {
+    val dataString: String = value.toWireString
+    val data: Array[Byte] = dataString.getBytes("UTF-8")
+    val header: Array[Byte] = "%06x".format(data.length).getBytes("UTF-8")
+    log.info("Writing: " + dataString)
+    out.write(header)
+    out.write(data)
+    out.flush()
+  }
+
+  private def fillArray(in: java.io.Reader, a: Array[Char]): Unit = {
+    var n = 0
+    val l = a.length
+    var charsRead = 0
+    while (n < l) {
+      charsRead = in.read(a, n, l - n)
+      if (charsRead == -1) {
+        throw new EOFException("End of file reached in socket reader.")
+      } else {
+        n += charsRead
+      }
+    }
+  }
+
+  private val headerBuf = new Array[Char](6)
+
+  override def readMessage(reader: java.io.InputStreamReader): WireFormat = {
+    fillArray(reader, headerBuf)
+    val msglen = Integer.valueOf(new String(headerBuf), 16).intValue()
+    if (msglen > 0) {
+      val buf: Array[Char] = new Array[Char](msglen)
+      fillArray(reader, buf)
+      SExpParser.read(new input.CharArrayReader(buf))
+    } else {
+      throw new IllegalStateException("Empty message read from socket!")
+    }
+  }
+
+}

rorygraves/ensime-server

build.sbt
@@ -145,7 +145,7 @@ graphSettings
 scalariformSettings
 
 // let's bump this every time we get more tests
-ScoverageKeys.coverageMinimum := 68
+ScoverageKeys.coverageMinimum :=73
 
 ScoverageKeys.coverageFailOnMinimum := true
 

src/main/scala/org/ensime/protocol/swank/SwankWireFormatCodec.scala
@@ -1,0 +1,60 @@
+package org.ensime.protocol.swank
+
+import java.io.{ EOFException, OutputStream }
+
+import org.ensime.protocol.ProtocolWireFormatCodec
+import org.ensime.util.{ SExpParser, WireFormat }
+import org.slf4j.Logger
+
+import scala.util.parsing.input
+
+trait SwankWireFormatCodec extends ProtocolWireFormatCodec {
+
+  val log: Logger
+
+  // Handle reading / writing of messages
+  override def writeMessage(value: WireFormat, out: OutputStream): Unit = {
+    val dataString: String = value.toWireString
+    val data: Array[Byte] = dataString.getBytes("UTF-8")
+    val header: Array[Byte] = "%06x".format(data.length).getBytes("UTF-8")
+
+    val displayStr = if (dataString.length > 3000)
+      dataString.take(3000) + "..."
+    else
+      dataString
+    log.info("Writing: " + displayStr)
+
+    out.write(header)
+    out.write(data)
+    out.flush()
+  }
+
+  private def fillArray(in: java.io.Reader, a: Array[Char]): Unit = {
+    var n = 0
+    val l = a.length
+    var charsRead = 0
+    while (n < l) {
+      charsRead = in.read(a, n, l - n)
+      if (charsRead == -1) {
+        throw new EOFException("End of file reached in socket reader.")
+      } else {
+        n += charsRead
+      }
+    }
+  }
+
+  private val headerBuf = new Array[Char](6)
+
+  override def readMessage(reader: java.io.InputStreamReader): WireFormat = {
+    fillArray(reader, headerBuf)
+    val msglen = Integer.valueOf(new String(headerBuf), 16).intValue()
+    if (msglen > 0) {
+      val buf: Array[Char] = new Array[Char](msglen)
+      fillArray(reader, buf)
+      SExpParser.read(new input.CharArrayReader(buf))
+    } else {
+      throw new IllegalStateException("Empty message read from socket!")
+    }
+  }
+
+}